e54203d05ab5c924f7331d7a3640c246bafb81d0
Meowyam <github@pandashop.co.uk>
2012-05-01 22:45:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crocusfields::Application.config.secret_token = '21f8582d6e55fa56dc24a159f3eda15cfe84b4091def5ce2b6dde34b38744bf591547dec0bc020bda6e2a1c3d98c14e3e0d72f5fef5f070e44ca7f3a90eef6ad'
