ca5ec55f822e6e2b131c89359d52aea995d39939
Meowyam <github@pandashop.co.uk>
2012-08-01 12:18:22 +0100

    Add user following. End of twitter with no hashtags

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,7 @@ describe "Static pages" do
   subject {page}
 
   describe "Home page" do
-      before { visit root_path}
-        it { should have_selector 'title', text: "Home" }
+      before { visit root_path }
 
       describe "for signed-in users" do
           let(:user) { FactoryGirl.create(:user) }
@@ -21,7 +20,18 @@ describe "Static pages" do
             user.feed.each do |item|
                 page.should have_selector("li##{item.id}", text: item.content)
             end
-        end
+          end
+
+          describe "follower/following counts" do
+              let(:other_user) { FactoryGirl.create(:user) }
+              before do
+                  other_user.follow!(user)
+                  visit root_path
+              end
+
+              it { should have_link("0 following", href: following_user_path(user)) }
+              it { should have_link("1 followers", href: followers_user_path(user)) }
+          end
       end
   end
 
