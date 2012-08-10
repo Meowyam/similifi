4dfc0d39fe71bd30d05c3b304c611f37e855fc75
Meowyam <github@pandashop.co.uk>
2012-08-10 12:39:03 +0100

    edited error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ActionMailer::Base
     default :from => "admin@pandashop.co.uk"
     def registration_confirmation(user)
-        @user = user
-        mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
+        mail(:to => user.email, :subject => "Registered")
     end
 end
