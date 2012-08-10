18d8e9f72a35ec5d906d18581bb39736f47495db
Meowyam <github@pandashop.co.uk>
2012-08-10 11:41:11 +0100

    fixed signed in page

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :user_name => "iamdoomkitty",
-    :password => "magister3ku64",
+    :user_name => "pandashoptest",
+    :password => "foobarfoobar",
     :authentication => "plain",
     :enable_starttls_auto => true
 }
