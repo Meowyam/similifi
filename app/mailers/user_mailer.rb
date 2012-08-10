bc4c39415b4839a73ffbeaf4b2a613ebd7f832df
Meowyam <github@pandashop.co.uk>
2012-08-10 13:12:45 +0100

    fixed mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
     default :from => "admin@pandashop.co.uk"
     def registration_confirmation(user)
+        @user = user
+        @url = "http://crocusfields.herokuapp.com"
         mail(:to => user.email, :subject => "Registered")
     end
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "gmail.com",
-    :user_name => "pandashoptest@gmail.com",
-    :password => "foobarfoobar",
+    :user_name => "iamdoomkitty@gmail.com",
+    :password => "magister3ku64",
     :authentication => "plain",
     :enable_starttls_auto => true
 }
