fe1c18b2ad71e100acbe1a51fea9d42409e6348e
Meowyam <github@pandashop.co.uk>
2013-01-07 16:12:42 +0000

    add mail setup with env vars

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+    :address => ENV['MAIL_DOMAIN'],
+    :port => 587,
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+    :domain => 'heroku.com'
+}
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.default_url_options[:host] = ENV['MAIL_HOST']
