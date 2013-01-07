254bd05c95f2832bfa7ef5716e4813088be039b0
Meowyam <github@pandashop.co.uk>
2013-01-07 02:34:10 +0000

    test6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Crocusfields::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => ENV['MAIL_HOST'] }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
